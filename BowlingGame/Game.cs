b4526a353eb8ce160b623251c0effd4e822abb29
frsanches@dev.com <sanches>
2024-07-08 21:04:06 +0200

    remove comments

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -2,13 +2,6 @@
 {
     public class Game
     {
-        // the game consists of 10 frames
-        // the player has two rolls to knock 10 pins in each frame
-        // the score for the frame is the total number of the pins knocked down plus bonuses for strikes? and spares?
-        // A spare is when the player knocks down all 10 pins in two rolls. The bonus for that frame is the number of pins knocked down by the next roll
-        // A strike is when the player knocks down all 10 pins on his first roll. The frame is then completed with a single roll. The bonus for that frame is the value of the next two rolls.
-        // In the tenth frame a player who rolls a spare or strike is allowed to roll the extra balls to complete the frame. However no more than three balls can be rolled in tenth frame.
-
         private Dictionary<int, int> frameStrike = new Dictionary<int, int>();
         private int[] frameScore = new int[10];
         private int rolls = 1;
